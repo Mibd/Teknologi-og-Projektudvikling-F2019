fe2da3bd3c2cd107d7f14a264e59dbc9d12a76f0
mibd <mibd@kea.dk>
2019-01-30 15:10:00 +0100

    Nu Git d30/1-19

---
+++ Arv Terning/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arv_Terning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Terning t1 = new Terning();
+
+            Console.WriteLine($"Start værdi t1 [{t1.Værdi}]");
+            //Console.Write("Press any key to continue . . . ");
+            //Console.ReadKey();
+            t1.Ryst();
+            //Console.WriteLine();
+            Console.WriteLine($"værdi efter ryst t1 [{t1.Værdi}]");
+
+            LudoTerning t2 = new LudoTerning();
+
+            Console.WriteLine($"Start værdi t2 [{t2.Værdi}]");
+            //Console.Write("Press any key to continue . . . ");
+            //Console.ReadKey();
+            t2.Ryst();
+            //Console.WriteLine();
+            Console.WriteLine($"værdi efter ryst t2 [{t2.Værdi}]");
+            Console.WriteLine($"Er Globus {t2.ErGlobus()}");
+            Console.WriteLine($"Er Stjerne {t2.ErStjerne()}");
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+
+    }
+    public class Terning
+    {
+        private static System.Random rnd;
+        private int værdi;
+
+        public int Værdi
+        {
+            get
+            {
+                return this.værdi;
+            }
+            set
+            {
+                if (value < 0 || value > 6)
+                    value = 1;
+                this.værdi = value;
+            }
+        }
+        public void Skriv()
+        {
+            Console.WriteLine("[" + this.Værdi + "]");
+        }
+        public void Ryst()
+        {
+            this.Værdi = rnd.Next(1, 7);
+        }
+
+        public Terning()
+        {
+            this.Ryst();
+        }
+
+        public Terning(int værdi)
+        {
+            this.Værdi = værdi;
+        }
+
+        static Terning()
+        {
+            rnd = new Random();
+        }
+    }
+    public class LudoTerning: Terning
+    {
+        public bool ErGlobus()
+
+        {
+            return this.Værdi == 3;
+        }
+
+        public bool ErStjerne()
+
+        {
+            return this.Værdi == 5;
+        }
+
+        public LudoTerning() : base()
+
+        { }
+
+        public LudoTerning(int værdi) : base(værdi)
+
+        { }
+
+    }
+}
---
+++ Regnemaskine/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regnemaskine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String String_var1 = "0"; //Opretter variablen
+            String String_var2 = "0";
+
+            Console.WriteLine("Indtast tal1"); //Skriver teksten på konsollen
+            String_var1 = Console.ReadLine();  //Henter det indtastede til String_var1
+            Console.WriteLine("Indtast tal2");
+            String_var2 = Console.ReadLine();
+
+            Double t1 = System.Convert.ToDouble(String_var1); //Konvertere String_var1 til Double (Real)
+            Double t2 = System.Convert.ToDouble(String_var2);
+
+            Double Resultat = t1 + t2;
+
+            Console.WriteLine($"Resultatet er = {Resultat:N2}"); //Skriver ud til konsollen. ($ betyder at der er indeholt en variabel), (N2 betyder tusindtals seperator)
+
+
+
+
+
+
+
+
+
+
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
